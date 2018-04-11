3af24d02ebdff9c610b95a84f47dada499995bdb
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2018-04-11 20:35:11 +0200

    FSM complete for single elevator

--- src/state_machine.erl
+++ src/state_machine.erl
@@ -1,14 +1,29 @@
 -module(state_machine).
 -export([start/0]).
 
+-define(DOOR_OPEN_TIMEOUT_MS, 3000).
+
+
+%TODO Should update data to send via network
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Elevator interface wrapper %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+set_motor_direction(Direction) ->
+  elevator_interface:set_motor_direction(pid_elevator_interface, Direction).
+set_door_open_light(State) ->
+  elevator_interface:set_door_open_light(pid_elevator_interface, State).
+set_floor_indicator(Floor_nr) ->
+  elevator_interface:set_floor_indicator(pid_elevator_interface, Floor_nr).
+
+
 start() ->
-  io:fwrite("Start state machine module\n"),
+  io:format("Start state machine module~n"),
   state_init().
 
 
 state_init() ->
+  io:format("State: init ~n"),
   pid_event_handler ! {state, intializing},
-  io:format("State: init~n"),
 
   receive
     {event, first_floor_passed} ->
@@ -19,44 +34,40 @@ state_init() ->
 
 
 state_idle() ->
-  %pid_event_manager ! {state, idle},
   io:format("State: idle~n"),
-
   receive
-    {event, first_floor_reached} ->
-      io:format("First floor reached ~n")
-    %moving ->
-    %  state_moving();
-    %order_processing ->
-
-%    new_order ->   ;
-
+    {event, new_order, Direction} ->
+      io:format("neworder~n"),
+      set_motor_direction(Direction),
+      state_moving(Direction);
+    {event, order_floor_reached} ->
+      set_motor_direction(stop),
+      state_open_door()
   after 1500 ->
     state_idle()
   end.
 
 
 state_open_door() ->
-  pid_event_manager ! {state, floor_reached},
+  io:format("Door open~n"),
+  set_door_open_light(on),
+  timer:sleep(?DOOR_OPEN_TIMEOUT_MS),
+  io:format("Door closed~n"),
+  set_door_open_light(off),
+  state_idle().
+
+
+state_moving(Direction) ->
+  io:format("State: Moving up ~n"),
   receive
-    floor_reached ->
-
-
-    state_idle()
+    {event, floor_detected, Floor_nr} ->
+      set_floor_indicator(Floor_nr),
+      set_motor_direction(stop),
+      state_idle()
+  after 1500 ->
+    state_moving(Direction)
   end.
 
-%state_moving() ->
-%  pid_event_manager ! {state, floor_reached},
-%  receive
-%    floor_reached ->
-%      1;
-%    order_processing ->
-%      state_order_processing();
-%    moving_up ->
-%      state_moving()
-%    after 1500 ->
-%      state_idle()
-%    end.
 
 
 
