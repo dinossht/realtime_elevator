a8f3000ca6a292d246654b50826983c53bf1044d
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2018-04-13 22:05:50 +0200

    Auto stash before merge of "delete" and "master"

--- src/elevator.erl
+++ src/elevator.erl
@@ -4,6 +4,8 @@
 start() ->
   {ok, Pid_elevator_interface} = elevator_interface:start(),
   register(pid_elevator_interface, Pid_elevator_interface),
+
   register(pid_event_handler, spawn(fun() -> event_handler:start() end)),
+  %register(pid_state_machine, spawn(fun() -> order_handler:start() end)),
   register(pid_state_machine, spawn(fun() -> state_machine:start() end)).
 
