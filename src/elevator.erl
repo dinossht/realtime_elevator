f19943e210161b232db574db0053cfbeedfdda9a
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2018-04-14 01:46:59 +0200

    Add data storage as a thread

--- src/elevator.erl
+++ src/elevator.erl
@@ -1,11 +1,15 @@
 -module(elevator).
 -export([start/0]).
 
+
 start() ->
-  data_storage:start(),
+
+  register(pid_data_storage, spawn(fun() -> data_storage:start() end)),
+
   {ok, Pid_elevator_interface} = elevator_interface:start(),
   register(pid_elevator_interface, Pid_elevator_interface),
   register(pid_event_handler, spawn(fun() -> event_handler:start() end)),
   %register(pid_state_machine, spawn(fun() -> order_handler:start() end)),
   register(pid_state_machine, spawn(fun() -> state_machine:start() end)).
+
 
