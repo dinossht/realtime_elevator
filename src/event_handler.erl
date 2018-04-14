19e956409900e26d45aa694b26a25d67f9c440da
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2018-04-14 15:36:42 +0200

    Fix order add, set order button light,

--- src/event_handler.erl
+++ src/event_handler.erl
@@ -6,6 +6,7 @@
 
 %TODO: During startup: set correct floor indicator, turn off lights etc.
 %TODO: Remember to not poll up on 4th floor up etc.
+%TODO: Fix prev_button maybe?
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Elevator interface wrapper %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -13,9 +14,6 @@ get_order_button_state(Floor_nr, Button_type) ->
   elevator_interface:get_order_button_state(pid_elevator_interface, Floor_nr, Button_type).
 get_floor_sensor_state() ->
   elevator_interface:get_floor_sensor_state(pid_elevator_interface).
-
-set_order_button_light(Button_type, Floor_nr, State) ->
-  elevator_interface:set_order_button_light(pid_elevator_interface, Button_type, Floor_nr, State).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 start() ->
@@ -32,6 +30,7 @@ event_handle_floorDetected() ->
     between_floors ->
       io:format("");
     _ ->
+      io:format("Floor reached~n"),
       pid_state_machine ! {floor_detected},
       pid_data_storage ! {current_floor_add, Floor_nr}
   end,
@@ -43,14 +42,13 @@ event_handle_cabButtonClick(Floor_nr) when Floor_nr > 3 ->
   event_handle_cabButtonClick(0);
 event_handle_cabButtonClick(Floor_nr) when Floor_nr =< 3 ->
   Button_state = get_order_button_state(Floor_nr, cab),
-  %io:format("Cab button running... state: ~p ~n", [Button_state]),
   Prev_state = Button_state, % = data_storage:prev_button_state_getStatus(Floor_nr, cab),
 
   %case (Button_state /= Prev_state) and (Button_state == 1) of
   case (Button_state == 1) of
     true ->
-      io:format("New cab click  "),
-      pid_data_storage ! {order_add, Floor_nr, cab, 1};
+      pid_data_storage ! {order_add, Floor_nr, cab, 1},
+      pid_state_machine ! {new_order};
     false -> timer:sleep(?ORDER_BUTTON_POLL_PERIOD_MS)
   end,
 
@@ -60,12 +58,13 @@ event_handle_upButtonClick(Floor_nr) when Floor_nr > 2 ->
   event_handle_upButtonClick(0);
 event_handle_upButtonClick(Floor_nr) when Floor_nr =< 2 ->
   Button_state = get_order_button_state(Floor_nr, hall_up),
-  %io:format("Up button running... state: ~p ~n", [Button_state]),
   Prev_state = Button_state, % = data_storage:prev_button_state_getStatus(Floor_nr, hall_up),
 
   %case (Button_state /= Prev_state) and (Button_state == 1) of
   case (Button_state == 1) of
-    true -> io:format("New up click");
+    true ->
+      pid_data_storage ! {order_add, Floor_nr, up, 1},
+      pid_state_machine ! {new_order};
     false -> timer:sleep(?ORDER_BUTTON_POLL_PERIOD_MS)
   end,
 
@@ -77,8 +76,11 @@ event_handle_downButtonClick(Floor_nr) when Floor_nr =< 3 ->
   Button_state = get_order_button_state(Floor_nr, hall_down),
   Prev_state = Button_state, % = data_storage:prev_button_state_getStatus(Floor_nr, hall_down),
 
-  case (Button_state /= Prev_state) and (Button_state == 1) of
-    true -> io:format("New down click");
+  %case (Button_state /= Prev_state) and (Button_state == 1) of
+  case (Button_state == 1) of
+    true ->
+      pid_data_storage ! {order_add, Floor_nr, down, 1},
+      pid_state_machine ! {new_order};
     false -> timer:sleep(?ORDER_BUTTON_POLL_PERIOD_MS)
   end,
 
