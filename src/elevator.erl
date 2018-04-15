6a401f3ddca415c7b81b08058031f43bdc3787b0
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2018-04-15 10:18:34 +0200

    Add node1, node2 start function

--- src/elevator.erl
+++ src/elevator.erl
@@ -1,9 +1,21 @@
 -module(elevator).
--export([start/0]).
+-export([start1/0, start2/0]).
 
 
-start() ->
-  {ok, Pid_elevator_interface} = elevator_interface:start(),
+start1() ->
+
+  network_manager:start1(),
+  {ok, Pid_elevator_interface} = elevator_interface:start1(),
+  register(pid_elevator_interface, Pid_elevator_interface),
+  register(pid_data_storage, spawn(fun() -> data_storage:start() end)),
+  register(pid_state_machine, spawn(fun() -> state_machine:start() end)),
+  register(pid_event_handler, spawn(fun() -> event_handler:start() end)).
+
+
+start2() ->
+
+  network_manager:start2(),
+  {ok, Pid_elevator_interface} = elevator_interface:start2(),
   register(pid_elevator_interface, Pid_elevator_interface),
   register(pid_data_storage, spawn(fun() -> data_storage:start() end)),
   register(pid_state_machine, spawn(fun() -> state_machine:start() end)),
